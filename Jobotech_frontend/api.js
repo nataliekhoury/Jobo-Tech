d7c50d8eaad813ef73b3dc081d4bdf5e7edf6063
nataliekhoury <nataliekhoury1122@gmail.com>
2024-01-19 13:51:25 +0200

    first commit

---
+++ Jobotech_frontend/api.js
@@ -0,0 +1,7 @@
+// api.js
+const API_BASE_URL = 'http://localhost:3000'; // Update with your NestJS server URL
+
+export const API_URLS = {
+  exampleEndpoint:`${API_BASE_URL}/example`,
+  // Add more endpoints as needed
+};
