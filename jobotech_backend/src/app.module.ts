a8e03ba9861a73473146f24723fa3575dbfbc3fd
Norhan308 <norhan.30.8@gmail.com>
2024-01-20 19:54:45 +0200

    add the DB URL

--- jobotech_backend/src/app.module.ts
+++ jobotech_backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: 
+  [
+    MongooseModule.forRoot('mongodb+srv://norhan308:JoboTech@cluster0.ginlzm6.mongodb.net/'),
+    ChatModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
